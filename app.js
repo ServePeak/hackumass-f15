961a2d7dacbbb0dbab7f06969bd08715883dc297
RatedRion <ratedrion@gmail.com>
2015-10-24 19:09:35 -0400

    bootstrap
    added bootstrap and moved files around

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/public/';
+var qs = require('./quickstart');
+
+var mongo = require('mongodb'),
+  Server = mongo.Server,
+  Db = mongo.Db;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+var db = new Db('exampleDb', server);
+
+db.open(function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  } else {
+		console.log("Fuck.");
+		}
+});
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -12,8 +28,8 @@ router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
 
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
+router.get("/test",function(req,res){
+	qs.fs.readfile();
 });
 
 router.get("/contact",function(req,res){
