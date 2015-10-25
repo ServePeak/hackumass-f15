7dd7d422183b06dc98cbb030958b15fa9cd40120
Yunbin Zhang <mzhangyb@gmail.com>
2015-10-24 23:56:10 -0400

    -a

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
+var mongouri = 'mongodb://localhost/users';
+var db = require('mongoose').connect(mongouri),
+    model = require('./app/models/user.server.model'),
+	control = require('./app/controllers/users.server.controller');
+
+var path = __dirname + '/public/';
+var qs = require('./quickstart');
 var express = require("express");
 var app = express();
 var router = express.Router();
-var path = __dirname + '/public/';
-var qs = require('./quickstart');
 
-var mongo = require('mongodb'),
+var bodyParser = require('body-parser');
+
+
+var port = 1024;
+
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}));
+
+app.use(bodyParser.json());
+
+app.use(express.static('./public'));
+//app.use(
+/*var mongo = require('mongodb'),
   Server = mongo.Server,
   Db = mongo.Db;
 
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = new Db('exampleDb', server);
-
-db.open(function(err, db) {
+*/
+/*db.open(function(err, db) {
   if(!err) {
     console.log("We are connected");
   } else {
 		console.log("Fuck.");
 		}
-});
+});*/
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -42,6 +61,8 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(80,function(){
-  console.log("Live at Port 80");
+app.listen(port,function(){
+  console.log("Live at Port 1024");
 });
+
+app.route('/users').post(control.create);
